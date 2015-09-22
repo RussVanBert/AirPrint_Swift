7fbe23b42f080060935362e5ff09a9116f56568e
Russell Van Bert <russ_vb@yahoo.com.au>
2015-09-22 22:06:19 +1000

    Why 'if let' when you can flatMap.

--- AirPrint_Swift/ViewController.swift
+++ AirPrint_Swift/ViewController.swift
@@ -4,29 +4,30 @@ class ViewController: UIViewController {
 
   @IBAction func print(sender: AnyObject) {
     let urlPath = "http://cdn2.raywenderlich.com/wp-content/uploads/2014/06/RW-Swift-Cheatsheet-0_3.pdf"
-    if let url = NSURL(string: urlPath) {
-      if (UIPrintInteractionController.canPrintURL(url)) {
-        showPrintInteraction(url)
-      } else {
-        println("Unable to print: \(url)")
-      }
+    let _ = NSURL(string: urlPath).flatMap{ printUrl($0) }
+  }
+  
+  func printUrl(url: NSURL) {
+    if (UIPrintInteractionController.canPrintURL(url)) {
+      showPrintInteraction(url)
+    } else {
+      Swift.print("Unable to print: \(url)")
     }
   }
   
   func showPrintInteraction(url: NSURL) {
-    if var controller = UIPrintInteractionController.sharedPrintController() {
-      controller.printingItem = url
-      controller.printInfo = printerInfo(url.lastPathComponent!)
-      controller.showsPageRange = true
-      controller.presentAnimated(true, completionHandler: nil)
-    }
+    let controller = UIPrintInteractionController.sharedPrintController()
+    controller.printingItem = url
+    controller.printInfo = printerInfo(url.lastPathComponent!)
+    controller.showsPageRange = true
+    controller.presentAnimated(true, completionHandler: nil)
   }
   
   func printerInfo(jobName: String) -> UIPrintInfo {
     let printInfo = UIPrintInfo.printInfo()
     printInfo.outputType = .General
     printInfo.jobName = jobName
-    println("Printing: \(jobName)")
+    Swift.print("Printing: \(jobName)")
     return printInfo
   }
 }
